211d6f8fc129c711a4a73674155f5590f4c66f31
Jabbar <abduljabbar.sha97@gmail.com>
2020-03-27 15:32:45 +0300

    [Icons_Images] : All needed Icons and Images for building Buy_layout are uploaded to start designing and the previous design is deleted.

--- app/src/main/java/com/example/inf024project/Secenekler.java
+++ app/src/main/java/com/example/inf024project/Secenekler.java
@@ -2,6 +2,7 @@ package com.example.inf024project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -18,13 +19,14 @@ public class Secenekler extends AppCompatActivity {
         go=findViewById(R.id.go_btn);
 
         // Buy butonuna basılınca yapılacak işler
-        /*
+
         buy.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                Intent intent=new Intent(Secenekler.this,buy.class);
+                startActivity(intent);
             }
-        });*/
+        });
 
         // Go butonuna basınca yapılacak isler
         /*
