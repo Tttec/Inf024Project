15efc4d3c99f9c8fb3fefc2e7e92ffea8fafc68c
JabSYsEmb <32222578+JabSYsEmb@users.noreply.github.com>
2020-03-29 00:50:00 -0700

    [Button] : Assign a button to go to the next Activity.

--- app/src/main/java/com/example/inf024project/BuyOrGo.java
+++ app/src/main/java/com/example/inf024project/BuyOrGo.java
@@ -4,10 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
-public class BuyOrGo extends AppCompatActivity {
-    Button buy,go;
+public class BuyOrGo extends AppCompatActivity implements View.OnClickListener{
+    public Button buy,go;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,7 +17,17 @@ public class BuyOrGo extends AppCompatActivity {
 
         buy = findViewById(R.id.buy_btn);
         go = findViewById(R.id.go_btn);
+        buy.setOnClickListener(this);
+        go.setOnClickListener(this);
     }
+    public void onClick(View v){
+        switch (v.getId()){
+            case R.id.buy_btn:
+                clickBuy_btn();
+            case R.id.go_btn:
+        }
+    }
+
         // Buy butonuna basılınca yapılacak işler
         /*
         buy.setOnClickListener(new View.OnClickListener() {
@@ -35,4 +46,9 @@ public class BuyOrGo extends AppCompatActivity {
 
             }
         });*/
+
+    public void clickBuy_btn(){
+        Intent intent = new Intent(this,HomePageOfBuyApp.class);
+        startActivity(intent);
     }
+}
