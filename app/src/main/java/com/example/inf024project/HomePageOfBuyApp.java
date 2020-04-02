2008e0c82a3df4e3614ffed5949204666fd657c8
JabSYsEmb <32222578+JabSYsEmb@users.noreply.github.com>
2020-04-01 21:06:50 -0700

    [Layout] : Notification_Page.

--- app/src/main/java/com/example/inf024project/HomePageOfBuyApp.java
+++ app/src/main/java/com/example/inf024project/HomePageOfBuyApp.java
@@ -2,13 +2,41 @@ package com.example.inf024project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.view.View;
+import android.widget.Button;
 
-public class HomePageOfBuyApp extends AppCompatActivity {
+public class HomePageOfBuyApp extends AppCompatActivity implements View.OnClickListener {
+
+    public Button profile_btn,bell_btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.buy_home_page);
+        profile_btn = findViewById(R.id.profile_btn);
+        bell_btn = findViewById(R.id.bell_btn);
+        profile_btn.setOnClickListener(this);
+        bell_btn.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case(R.id.profile_btn):
+                profile_bt();
+            case(R.id.bell_btn):
+                bell_btn();
+        }
+    }
+    public void profile_bt(){
+        Intent intent = new Intent(this, ProfilePage.class);
+        startActivity(intent);
+    }
+    public void bell_btn(){
+        Intent intent = new Intent(this, NotificationPage.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/inf024project/NotificationPage.java
+++ app/src/main/java/com/example/inf024project/NotificationPage.java
@@ -4,12 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class ProfilePage extends AppCompatActivity {
+public class NotificationPage extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.profile_page);
-
+        setContentView(R.layout.notification_page);
     }
 }
