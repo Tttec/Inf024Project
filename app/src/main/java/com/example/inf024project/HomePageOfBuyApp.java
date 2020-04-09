06821aa7b58150dde0c591c86e4da837f4d20465
ibrahim-nemmura <ibrahim.nemmura@gmail.com>
2020-04-09 20:15:05 +0300

    profile and notification buttons have been activated

--- app/src/main/java/com/example/inf024project/HomePageOfBuyApp.java
+++ app/src/main/java/com/example/inf024project/HomePageOfBuyApp.java
@@ -1,41 +1,45 @@
 package com.example.inf024project;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
-import android.provider.ContactsContract;
 import android.view.View;
 import android.widget.Button;
 
-public class HomePageOfBuyApp extends AppCompatActivity implements View.OnClickListener {
+public class HomePageOfBuyApp extends AppCompatActivity {
 
-    public Button profile_btn,bell_btn;
+    private Button profile_btn;
+    private Button bell_btn ;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.buy_home_page);
-        profile_btn = findViewById(R.id.profile_btn);
-        bell_btn = findViewById(R.id.bell_btn);
-        profile_btn.setOnClickListener(this);
-        bell_btn.setOnClickListener(this);
+        //Activate profile button
+        profile_btn = (Button) findViewById(R.id.profile_btn);
+        profile_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openProfilepage();
+            }
+        });
+
+        //Activate notifications button
+        bell_btn = (Button) findViewById(R.id.bell_btn);
+        bell_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openNotificationPage();
+            }
+        });
+    }
+    public void openProfilepage(){
+        Intent intent = new Intent(this, ProfilePage.class );
+                startActivity(intent);
     }
 
-    @Override
-    public void onClick(View v) {
-        switch (v.getId()){
-            case(R.id.profile_btn):
-                profile_bt();
-            case(R.id.bell_btn):
-                bell_btn();
-        }
-    }
-    public void profile_bt(){
-        Intent intent = new Intent(this, ProfilePage.class);
-        startActivity(intent);
-    }
-    public void bell_btn(){
+    public void openNotificationPage(){
         Intent intent = new Intent(this, NotificationPage.class);
         startActivity(intent);
     }
