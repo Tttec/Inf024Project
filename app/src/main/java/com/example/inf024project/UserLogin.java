899444fd55824440c60e927ed7f5823e079aea14
Sait <e160501132@stud.tau.edu.tr>
2020-04-10 14:17:57 +0300

    Database eklendi. Sait

--- app/src/main/java/com/example/inf024project/UserLogin.java
+++ app/src/main/java/com/example/inf024project/UserLogin.java
@@ -4,30 +4,43 @@ package com.example.inf024project;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
 
-public class UserLogin extends AppCompatActivity {
+public class UserLogin extends AppCompatActivity implements View.OnClickListener{
 
     private Button login;
+    private EditText loginEmail, loginPass;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.user_login);
         login = (Button) findViewById(R.id.login_login_options);
-        EditText loginEmail = (EditText) findViewById(R.id.login_email);
-        EditText loginPass = (EditText) findViewById(R.id.login_pass);
-
-        //login.setOnClickListener(this);
-
-        }
-
-
-
+        loginEmail = (EditText) findViewById(R.id.login_email);
+        loginPass = (EditText) findViewById(R.id.login_pass);
+        login.setOnClickListener(this);
 
     }
+
+    @Override
+    public void onClick(View v){
+        OnLogin(v);
+    }
+
+    public void OnLogin(View view) {
+
+    String username = (String) loginEmail.getText().toString();
+    String password = (String) loginPass.getText().toString();
+    String type = "login";
+    DatabaseConnection db = new DatabaseConnection(this);
+    db.execute(type, username, password);
+    }
+
+
+}
 
 
 
